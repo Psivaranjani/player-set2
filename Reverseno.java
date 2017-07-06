67a5353e727ffd766d7c8823a20ad9d584726eab
Psivaranjani <sathyapalanichamy90@gmail.com>
2017-07-06 14:37:13 +0530

    Create Reverseno.java

---
+++ Reverseno.java
@@ -0,0 +1,20 @@
+# player-set2
+import java.util.*;
+    public class Reverseno
+    {
+      public static void main(String args[])
+      {
+        int n, m, a = 0,x;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter any number:");
+        n = s.nextInt();
+        m = n;
+        while(n > 0)
+        {  
+          x = n % 10;
+          a = a * 10 + x;
+          n = n / 10;
+        }
+      System.out.println(a);
+  }
+}
